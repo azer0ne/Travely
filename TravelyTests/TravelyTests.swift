79c46d0dc7122d5e0a9148fe7d44596296330c23
Abiyyu Firmansyah <abiyyu.firmansyah@mncgroup.com>
2026-04-01 13:25:48 +0700

    Initial Commit

---
+++ TravelyTests/TravelyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelyTests.swift
+//  TravelyTests
+//
+//  Created by Reza on 01/04/26.
+//
+
+import Testing
+@testable import Travely
+
+struct TravelyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
